df1e9d1a9387aa38c6eae7fd7e9a17ae20941db4
mig <thefiregod2511@gmail.com>
2021-05-04 18:04:04 +1000

    Revert "prettify entire repo"
    This reverts commit 90ba7684d8c0b1b61f0748318803782e8a0f0a00.

--- src/Square.js
+++ src/Square.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Square extends Component {
-	render() {
-		let e = this.props.on.find(
-			o => o.posX === this.props.posX && o.posY === this.props.posY
-		);
-		if (e) {
-			e = e.direction;
-		} else {
-			e = undefined;
-		}
-		return (
-			<div
-				className={`${
-					this.props.red[0] === this.props.posX &&
-					this.props.red[1] === this.props.posY
-						? 'red'
-						: e
-						? ''
-						: 'box'
-				} ${e === 0 || e === 2 ? 'dark-north' : ''} ${
-					e === 1 || e === 3 ? 'dark-east' : ''
-				}`}
-				onClick={this.props.onClick}
-			></div>
-		);
-	}
+  render() {
+    return (
+      <div
+        className={
+          this.props.on.some(
+            (o) => o.posX === this.props.posX && o.posY === this.props.posY
+          )
+            ? "dark"
+            : this.props.red[0] === this.props.posX &&
+              this.props.red[1] === this.props.posY
+            ? "red"
+            : "box"
+        }
+        onClick={this.props.onClick}
+      ></div>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,249 +1,244 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Square from './Square';
-import './index.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Square from "./Square";
+import "./index.css";
 
 class Game extends Component {
-	constructor(props) {
-		super(props);
-		this.handleKeyDown = this.handleKeyDown.bind(this);
-		this.state = {
-			on: [
-				{
-					posX: 7,
-					posY: 8,
-					direction: 0,
-				},
-				{
-					posX: 7,
-					posY: 7,
-					direction: 0,
-				},
-			],
-			red: [],
-			direction: 0,
-			disabled: false,
-			score: 0,
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.state = {
+      on: [
+        {
+          posX: 7,
+          posY: 8,
+          direction: 0,
+        },
+        {
+          posX: 7,
+          posY: 7,
+          direction: 0,
+        },
+      ],
+      red: [],
+      direction: 0,
+      disabled: false,
+      score: 0,
+    };
+  }
 
-	getRandomPos() {
-		const e = [Math.floor(Math.random() * 15), Math.floor(Math.random() * 15)];
-		if (this.state.on.some(o => o.posX === e[0] && o.posY === e[1]))
-			return this.getRandomPos();
-		return e;
-	}
+  getRandomPos() {
+    const e = [Math.floor(Math.random() * 15), Math.floor(Math.random() * 15)];
+    if (this.state.on.some((o) => o.posX === e[0] && o.posY === e[1])) return this.getRandomPos();
+    return e;
+  }
 
-	handleScoreChange() {
-		this.setState(prevState => ({
-			score: prevState.score + 1,
-		}));
-		if (
-			!localStorage.getItem('score') ||
-			localStorage.getItem('score') < this.state.score
-		)
-			localStorage.setItem('score', this.state.score);
-	}
+  handleScoreChange() {
+    this.setState((prevState) => ({
+      score: prevState.score + 1,
+    }));
+    if (
+      !localStorage.getItem("score") ||
+      localStorage.getItem("score") < this.state.score
+    )
+      localStorage.setItem("score", this.state.score);
+  }
 
-	addPos(x, y) {
-		if (x === -1 || x === 15 || y === -1 || y === 15) {
-			return this.disable();
-		}
-		if (this.state.red[0] === x && this.state.red[1] === y) {
-			this.handleScoreChange();
-			return this.addPosFromOppDirection();
-		}
-		if (this.includes(x, y)) return this.disable();
-		if (this.state.disabled === true) return;
-		this.setState(prevState => ({
-			on: [
-				...prevState.on,
-				{ posX: x, posY: y, direction: this.state.direction },
-			],
-		}));
-	}
+  addPos(x, y) {
+    if (x === -1 || x === 15 || y === -1 || y === 15) {
+      return this.disable();
+    }
+    if (this.state.red[0] === x && this.state.red[1] === y) {
+      this.handleScoreChange();
+      return this.addPosFromOppDirection();
+    }
+    if (this.includes(x, y)) return this.disable();
+    if (this.state.disabled === true) return;
+    this.setState((prevState) => ({
+      on: [
+        ...prevState.on,
+        { posX: x, posY: y, direction: this.state.direction },
+      ],
+    }));
+  }
 
-	addOpPos(x, y) {
-		if (x === -1 || x === 15 || y === -1 || y === 15) {
-			return this.disable();
-		}
-		this.setState({
-			red: [Math.floor(Math.random() * 15), Math.floor(Math.random() * 15)],
-		});
-		this.setState(prevState => ({
-			on: [
-				{ posX: x, posY: y, direction: this.state.direction },
-				...prevState.on,
-			],
-		}));
-		this.addPosFromDirection();
-	}
+  addOpPos(x, y) {
+    if (x === -1 || x === 15 || y === -1 || y === 15) {
+      return this.disable();
+    }
+    this.setState({
+      red: [Math.floor(Math.random() * 15), Math.floor(Math.random() *15)],
+    });
+    this.setState((prevState) => ({
+      on: [
+        { posX: x, posY: y, direction: this.state.direction },
+        ...prevState.on,
+      ],
+    }));
+    this.addPosFromDirection();
+  }
 
-	includes(x, y) {
-		if (this.state.on.some(e => e.posX === x && e.posY === y)) {
-			return true;
-		}
-	}
+  includes(x, y) {
+    if (this.state.on.some((e) => e.posX === x && e.posY === y)) {
+      return true;
+    }
+  }
 
-	disable() {
-		this.setState({
-			disabled: true,
-		});
-	}
+  disable() {
+    this.setState({
+      disabled: true,
+    });
+  }
 
-	addPosFromDirection() {
-		const x = this.state.on[this.state.on.length - 1];
-		switch (this.state.direction) {
-			case 0:
-				this.addPos(x.posX, x.posY - 1);
-				break;
-			case 1:
-				this.addPos(x.posX + 1, x.posY);
-				break;
-			case 2:
-				this.addPos(x.posX, x.posY + 1);
-				break;
-			case 3:
-				this.addPos(x.posX - 1, x.posY);
-				break;
-			default:
-				return new Error('There is no direction in the state');
-		}
-	}
+  addPosFromDirection() {
+    const x = this.state.on[this.state.on.length - 1];
+    switch (this.state.direction) {
+      case 0:
+        this.addPos(x.posX, x.posY - 1);
+        break;
+      case 1:
+        this.addPos(x.posX + 1, x.posY);
+        break;
+      case 2:
+        this.addPos(x.posX, x.posY + 1);
+        break;
+      case 3:
+        this.addPos(x.posX - 1, x.posY);
+        break;
+      default:
+        return new Error("There is no direction in the state");
+    }
+  }
 
-	addPosFromOppDirection() {
-		const x = this.state.on[0];
-		switch (x.direction) {
-			case 0:
-				this.addOpPos(x.posX, x.posY + 1);
-				break;
-			case 1:
-				this.addOpPos(x.posX - 1, x.posY);
-				break;
-			case 2:
-				this.addOpPos(x.posX, x.posY - 1);
-				break;
-			case 3:
-				this.addOpPos(x.posX + 1, x.posY);
-				break;
-			default:
-				return new Error('There is no direction in the state');
-		}
-	}
+  addPosFromOppDirection() {
+    const x = this.state.on[0];
+    switch (x.direction) {
+      case 0:
+        this.addOpPos(x.posX, x.posY + 1);
+        break;
+      case 1:
+        this.addOpPos(x.posX - 1, x.posY);
+        break;
+      case 2:
+        this.addOpPos(x.posX, x.posY - 1);
+        break;
+      case 3:
+        this.addOpPos(x.posX + 1, x.posY);
+        break;
+      default:
+        return new Error("There is no direction in the state");
+    }
+  }
 
-	removeOldestPos() {
-		if (this.state.disabled === true) return;
-		this.setState(prevState => ({
-			on: [...prevState.on.slice(1)],
-		}));
-	}
+  removeOldestPos() {
+    if (this.state.disabled === true) return;
+    this.setState((prevState) => ({
+      on: [...prevState.on.slice(1)],
+    }));
+  }
 
-	changeDirection(e) {
-		this.setState(prevState => ({
-			direction: (prevState.direction + e) % 4,
-		}));
-	}
+  changeDirection(e) {
+    this.setState((prevState) => ({
+      direction: (prevState.direction + e) % 4,
+    }));
+  }
 
-	handleKeyDown(e) {
-		// if (this.state.disabled === true) return;
-		switch (e.keyCode) {
-			case 37: // left / a
-			case 65:
-				if (this.state.direction === 0 || this.state.direction === 2)
-					this.changeDirection(this.state.direction === 0 ? 3 : 1);
-				break;
-			case 38: // up / w
-			case 87:
-				if (this.state.direction === 3 || this.state.direction === 1)
-					this.changeDirection(this.state.direction === 3 ? 1 : 3);
-				break;
-			case 39: // right / d
-			case 68:
-				if (this.state.direction === 0 || this.state.direction === 2)
-					this.changeDirection(this.state.direction === 0 ? 1 : 3);
-				break;
-			case 40: // down / s
-			case 83:
-				if (this.state.direction === 3 || this.state.direction === 1)
-					this.changeDirection(this.state.direction === 3 ? 3 : 1);
-				break;
-			default:
-				return;
-		}
-	}
+  handleKeyDown(e) {
+    // if (this.state.disabled === true) return;
+    switch (e.keyCode) {
+      case 37: // left / a
+      case 65:
+        if (this.state.direction === 0 || this.state.direction === 2)
+          this.changeDirection(this.state.direction === 0 ? 3 : 1);
+        break;
+      case 38: // up / w
+      case 87:
+        if (this.state.direction === 3 || this.state.direction === 1)
+          this.changeDirection(this.state.direction === 3 ? 1 : 3);
+        break;
+      case 39: // right / d
+      case 68:
+        if (this.state.direction === 0 || this.state.direction === 2)
+          this.changeDirection(this.state.direction === 0 ? 1 : 3);
+        break;
+      case 40: // down / s
+      case 83:
+        if (this.state.direction === 3 || this.state.direction === 1)
+          this.changeDirection(this.state.direction === 3 ? 3 : 1);
+        break;
+      default:
+        return;
+    }
+  }
 
-	componentDidMount() {
-		this.setState({
-			red: this.getRandomPos(),
-		});
-		setTimeout(() => this.timeout(), 100);
-		document.addEventListener('keydown', this.handleKeyDown);
-	}
+  componentDidMount() {
+    this.setState({
+      red: this.getRandomPos(),
+    })
+    setTimeout(() => this.timeout(), 100);
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
 
-	timeout() {
-		if (this.state.disabled === true) return;
-		this.addPosFromDirection();
-		this.removeOldestPos();
-		setTimeout(() => this.timeout(), 120);
-	}
+  timeout() {
+    if (this.state.disabled === true) return;
+    this.addPosFromDirection();
+    this.removeOldestPos();
+    setTimeout(() => this.timeout(), 120);
+  }
 
-	restart() {
-		console.log('restarting');
-		if (this.disabled === false) return;
-		console.log(this.state);
-		this.setState(
-			{
-				on: [
-					{
-						posX: 7,
-						posY: 8,
-						direction: 0,
-					},
-					{
-						posX: 7,
-						posY: 7,
-						direction: 0,
-					},
-				],
-				red: [Math.floor(Math.random() * 15), Math.floor(Math.random() * 15)],
-				direction: 0,
-				disabled: false,
-				score: 0,
-			},
-			() => this.timeout()
-		);
-	}
+  restart() {
+    console.log("restarting")
+    if (this.disabled === false) return;
+    console.log(this.state);
+    this.setState({
+      on: [
+        {
+          posX: 7,
+          posY: 8,
+          direction: 0,
+        },
+        {
+          posX: 7,
+          posY: 7,
+          direction: 0,
+        },
+      ],
+      red: [Math.floor(Math.random() * 15), Math.floor(Math.random() * 15)],
+      direction: 0,
+      disabled: false,
+      score: 0,
+    }, () => this.timeout());
+  }
 
-	render() {
-		const items = [];
-		for (let i = 0; i < 15; i++) {
-			for (let e = 0; e < 15; e++) {
-				items.push(
-					<Square posX={e} posY={i} on={this.state.on} red={this.state.red} />
-				);
-			}
-		}
-		return (
-			<div className="game">
-				Score: {this.state.score}
-				<br />
-				High Score: {localStorage.getItem('score') || this.state.score}
-				<div className="layout">
-					<div className="game-board">{items}</div>
-					<button
-						type="button"
-						className={
-							this.state.disabled === true ? 'text-block1' : 'text-block'
-						}
-						onClick={() => this.restart()}
-					>
-						<p>You died</p>
-						<p className="smaller-text">Click to respawn</p>
-					</button>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const items = [];
+    for (let i = 0; i < 15; i++) {
+      for (let e = 0; e < 15; e++) {
+        items.push(
+          <Square posX={e} posY={i} on={this.state.on} red={this.state.red} />
+        );
+      }
+    }
+    return (
+      <div className="game">
+        Score: {this.state.score}
+        <br />
+        High Score: {localStorage.getItem("score") || this.state.score}
+        <div className="layout">
+          <div className="game-board">{items}</div>
+          <button type="button"
+            className={
+              this.state.disabled === true ? "text-block1" : "text-block"
+            }
+            onClick={ () => this.restart()}
+          >
+            <p>You died</p>
+            <p className="smaller-text">Click to respawn</p>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById("root"));
