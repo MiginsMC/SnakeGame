90ba7684d8c0b1b61f0748318803782e8a0f0a00
mig <thefiregod2511@gmail.com>
2021-05-04 18:00:48 +1000

    prettify entire repo

--- src/Square.js
+++ src/Square.js
@@ -1,21 +1,29 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class Square extends Component {
-  render() {
-    return (
-      <div
-        className={
-          this.props.on.some(
-            (o) => o.posX === this.props.posX && o.posY === this.props.posY
-          )
-            ? "dark"
-            : this.props.red[0] === this.props.posX &&
-              this.props.red[1] === this.props.posY
-            ? "red"
-            : "box"
-        }
-        onClick={this.props.onClick}
-      ></div>
-    );
-  }
+	render() {
+		let e = this.props.on.find(
+			o => o.posX === this.props.posX && o.posY === this.props.posY
+		);
+		if (e) {
+			e = e.direction;
+		} else {
+			e = undefined;
+		}
+		return (
+			<div
+				className={`${
+					this.props.red[0] === this.props.posX &&
+					this.props.red[1] === this.props.posY
+						? 'red'
+						: e
+						? ''
+						: 'box'
+				} ${e === 0 || e === 2 ? 'dark-north' : ''} ${
+					e === 1 || e === 3 ? 'dark-east' : ''
+				}`}
+				onClick={this.props.onClick}
+			></div>
+		);
+	}
 }
